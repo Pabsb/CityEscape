578bc9f2cf1e0953b2c94e6b48b2bc39d66b7fa1
JoshuaMorin314 <jmorin@luc.edu>
2024-03-14 17:59:25 -0500

    combining files

--- hitboxPrototype.py
+++ hitboxPrototype.py
@@ -4,96 +4,95 @@ import pygame
 pygame.init()
 
 # Set up the screen
-width,height=800,600
-window=pygame.display.set_mode((width,height))
+window = pygame.display.set_mode((1280, 720))
 pygame.display.set_caption("City Escape")
 
 # Colors
-white=(255,255,255)
-grey=(127,127,127)
-black=(0,0,0)
-blue=(0,191,0)
+white = (255,255,255)
+grey = (127,127,127)
+black = (0,0,0)
+blue = (0,191,0)
 #invisible=(225,225,225,0)
 
 #useful constants
-speed=5
+speed = 5
 
 # Define player class
 class Player:
-    def __init__(self,w,h):
-        self.rect=pygame.Rect((width-w)//2,(height-h)//2,w,h)
-        self.rect.center=(width//2,height//2)
+    def __init__(self, w, h):
+        self.rect = pygame.Rect((window.get_width() - w) / 2, (window.get_height() - h) / 2, w, h)
+        self.rect.center = (window.get_width() / 2, window.get_height() / 2)
         #self.color=white
         #self.v=pygame.Vector2()
         #self.v[:]=0,0
     def draw(self):
         #pygame.draw.rect(window,self.color,self.rect)
-        pygame.draw.rect(window,white,self.rect)
-    def move(self,dx,dy,walls):
-        self.rect.x+=dx #self.v[0]
-        self.rect.y+=dy #self.v[1]
+        pygame.draw.rect(window, white, self.rect)
+    def move(self, dx, dy, walls):
+        self.rect.x += dx #self.v[0]
+        self.rect.y += dy #self.v[1]
         self.check_collisions()
         walls.check_collisions(self)
     def check_collisions(self):
         # check the border of the screen
-        if self.rect.top<0:                 # top border of the window
-            self.rect.top=0
-        if self.rect.bottom>height:         # bottom border of the window
-            self.rect.bottom=height
-        if self.rect[0]<0:                  # left border of the window
-            self.rect[0]=0;
-        if width<self.rect[0]+self.rect[2]: # left border of the window
-            self.rect[0]=width-self.rect[2]
+        if self.rect.top < 0:                 # top border of the window
+            self.rect.top = 0
+        if self.rect.bottom > window.get_height():         # bottom border of the window
+            self.rect.bottom = window.get_height()
+        if self.rect[0] < 0:                  # left border of the window
+            self.rect[0] = 0;
+        if window.get_width() < self.rect[0]+self.rect[2]: # left border of the window
+            self.rect[0] = window.get_width() - self.rect[2]
 
 class Walls:
     def __init__(self):
-        self.values=[]
-    def add(self,x,y,w,h):
-        self.values.append(pygame.Rect(x,y,w,h))
+        self.values = []
+    def add(self, x, y, w, h):
+        self.values.append(pygame.Rect(x, y, w, h))
     def draw(self):
         for i in range(len(self.values)):
             pygame.draw.rect(window,grey,self.values[i])
     def check_collisions(self,player):
-        p=player.rect
-        colliding=p.collidelistall(self.values) # find all wall collisions
+        p = player.rect
+        colliding = p.collidelistall(self.values) # find all wall collisions
         for wall_i in range(len(colliding)): # for each wall collision
             # determine the direction and distance needed to snap the outer edges together
-            r=self.values[colliding[0]]
-            to=[r.x-p.x-p.w,r.x+r.w-p.x,r.y-p.y-p.h,r.y+r.h-p.y] # distance to move to the [left, right, top, bottom] side of r
-            dir=min(range(len(to)),key=lambda i:abs(to[i])) # which index of to holds the shortest distance
+            r = self.values[colliding[0]]
+            to = [r.x-p.x-p.w,r.x+r.w-p.x,r.y-p.y-p.h,r.y+r.h-p.y] # distance to move to the [left, right, top, bottom] side of r
+            dir = min(range(len(to)), key = lambda i:abs(to[i])) # which index of to holds the shortest distance
             # resolve collision
-            if dir//2==0: # change to the x-coordinate needed
-                p.x+=to[dir]
+            if dir //2 == 0: # change to the x-coordinate needed
+                p.x += to[dir]
             else: # change to the x-coordinate needed
-                p.y+=to[dir]
+                p.y += to[dir]
 
 # initialize important variables
-walls=Walls()
-walls.add(50,100,100,400)
-walls.add(650,100,100,400)
-player=Player(40,80)
+walls = Walls()
+walls.add(0, 0, 100, window.get_height())
+walls.add(window.get_width() - 100, 0, 100, window.get_height())
+player = Player(40, 80)
 
-def main(walls,player):
+def main(walls, player):
     # setup
-    clock=pygame.time.Clock()
-    running=True
+    clock = pygame.time.Clock()
+    running = True
     # game loop
     while running:
         # end game
         for event in pygame.event.get():
-            if event.type==pygame.QUIT:
-                running=False
+            if event.type == pygame.QUIT:
+                running = False
         # get input
-        keys=pygame.key.get_pressed()
+        keys = pygame.key.get_pressed()
 
         if keys[pygame.K_w]:
-            player.move(0,-speed,walls)
+            player.move(0, -speed, walls)
         if keys[pygame.K_s]:
-            player.move(0,speed,walls)
+            player.move(0, speed, walls)
         if keys[pygame.K_d]:
-            player.move(speed,0,walls)
+            player.move(speed, 0, walls)
         if keys[pygame.K_a]:
-            player.move(-speed,0,walls)
+            player.move(-speed, 0, walls)
         # Drawing
         window.fill(black)
         walls.draw()
@@ -105,4 +104,4 @@ def main(walls,player):
     pygame.quit()
 
 if __name__ == "__main__":
-    main(walls,player)
+    main(walls, player)
