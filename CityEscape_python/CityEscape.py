1de32aaeaab03237141f25828229baeb133d2cc5
JoshuaMorin314 <jmorin@luc.edu>
2024-03-21 15:38:01 -0500

    added Mr Rat

--- CityEscape_python/CityEscape.py
+++ CityEscape_python/CityEscape.py
@@ -4,12 +4,16 @@ import pygame, random, sys, os
 import pygame.event as EVENTS
 
 # important variables
-player_speed=5
+gravity=5
+player_speed=7
+player_jump_strength=35
 train_speed=10
 is_train_moving=False
-input_delay=200
+delay=200
 last_input=0
 sprite_scl=3
+floor=572
+sprite_activity=1000
 
 # variables for pygame window - space invaders vertical screen style
 winWidth=960
@@ -38,62 +42,116 @@ clock=pygame.time.Clock()
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        # load standing still image
-        self.image=pygame.transform.scale_by(player_img,sprite_scl) # load and scale player sprite image
-        self.image.set_colorkey(WHITE) # set colorkey to remove black background for player's rect
+        # load images
+        img=pygame.image.load(os.path.join(img_dir,'DP_still.png')).convert()
+        self.imgs=[pygame.transform.scale_by(img,sprite_scl)]
+        for i in range(3):
+            img=pygame.image.load(os.path.join(img_dir,'DP_walk_left{}.png'.format(i))).convert()
+            self.imgs.insert(0,pygame.transform.scale_by(img,sprite_scl))
+            img=pygame.image.load(os.path.join(img_dir,'DP_walk_right{}.png'.format(i))).convert()
+            self.imgs.append(pygame.transform.scale_by(img,sprite_scl))
+        for img in self.imgs:
+            img.set_colorkey(WHITE)
+        # set image (standing still at start)
+        self.state=[3,0] # standing still by default
+        self.image=self.imgs[self.state[0]] # player sprite image
         self.rect=self.image.get_rect()
-        self.rect.center=(winWidth/2,winHeight/2)
         # set hitbox
-        print(self.rect)
         self.offset=14
         r=self.rect
         self.hitbox=pygame.Rect(r.x+r.w*0.33,r.y+r.h*0.14,r.w*0.34,r.w*0.68)
-        self.hitbox.center=self.rect.center
-        self.hitbox.centery+=self.offset
+        self.hitbox.center=(winWidth/2,floor-self.hitbox.h/2)
+        self.rect.center=(self.hitbox.centerx,self.rect.centery-self.offset)
         #pygame.draw.rect(self.image,RED,self.hitbox.copy().move(-self.rect.x,-self.rect.y),width=1)
         #self.hitbox=pygame.Rect(r.x+r.w*0.3,r.y+r.h*0.25,r.w*0.4,r.w*0.65)
         # set default speed
         self.speed_x=0
         self.speed_y=0
+        # times of last player events
+        self.last_step=0
+        self.last_jump=0
     # update per loop iteration
     def update(self):
-        self.speed_x=0
-        self.speed_y=0
         # check key presses
+        now=pygame.time.get_ticks()
         key_state=pygame.key.get_pressed()
         if key_state[pygame.K_LEFT]:# or key_state[pygame.K_a]:
-            self.speed_x=-5
-        if key_state[pygame.K_RIGHT]:# or key_state[pygame.K_d]:
-            self.speed_x=5
+            self.speed_x=-player_speed
+        elif key_state[pygame.K_RIGHT]:# or key_state[pygame.K_d]:
+            self.speed_x=player_speed
+        else:
+            self.speed_x=0
+        if self.hitbox.bottom<floor:
+            self.speed_y+=gravity
+        elif now-self.last_jump>2*delay and key_state[pygame.K_UP]:# or key_state[pygame.K_w]:
+            self.speed_y=-player_jump_strength
+            self.last_jump=now
+        """
         if key_state[pygame.K_UP]:# or key_state[pygame.K_w]:
             self.speed_y=-5
         if key_state[pygame.K_DOWN]:# or key_state[pygame.K_s]:
             self.speed_y=5
+        """
+        if self.speed_x==0:
+            self.state=[3,0]
+        elif self.speed_x>0:
+            if self.state[0]<4:
+                self.state=[4,0]
+            elif now-self.last_step>delay:
+                self.last_step=now
+                if self.state[1]%4<2:
+                    self.state[0]+=1
+                else:
+                    self.state[0]-=1
+                self.state[1]+=1
+        elif self.speed_x<0:
+            if self.state[0]>2:
+                self.state=[2,0]
+            elif now-self.last_step>delay:
+                self.last_step=now
+                if self.state[1]%4>1:
+                    self.state[0]+=1
+                else:
+                    self.state[0]-=1
+                self.state[1]+=1
+        self.image=self.imgs[self.state[0]]
         self.hitbox.x+=self.speed_x
         self.hitbox.y+=self.speed_y
         if self.hitbox.right>winWidth:
             self.hitbox.right=winWidth
         if self.hitbox.left<0:
             self.hitbox.left=0
-        if self.hitbox.bottom>winHeight:
-            self.hitbox.bottom=winHeight
-        if self.hitbox.top<0:
+        if self.hitbox.bottom>floor: #winHeight:
+            self.hitbox.bottom=floor #winHeight
+            self.speed_y=0
+        if self.hitbox.top<0: # shouldn't be possible but just in case
             self.hitbox.top=0
         self.rect.center=self.hitbox.center
         self.rect.centery-=self.offset
 
 # create a generic enemy sprite for the game - standard name is *mob*
 class Mob(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self,tag):
         pygame.sprite.Sprite.__init__(self)
         # set pristine original image for sprite object - random choice from list
-        self.image_original=random.choice(npc_imgs)
-        self.image_original.set_colorkey(WHITE)  # set colour key for original image
-        self.image=pygame.transform.scale_by(self.image_original.copy(),sprite_scl) # set scaled copy image for sprite rendering
+        img=pygame.image.load(os.path.join(img_dir,'{}_still.png'.format(tag))).convert()
+        self.imgs=[pygame.transform.scale_by(img,sprite_scl)]
+        for i in range(2):
+            img=pygame.image.load(os.path.join(img_dir,'{}_walk_left{}.png'.format(tag,i))).convert()
+            self.imgs.insert(0,pygame.transform.scale_by(img,sprite_scl))
+            img=pygame.image.load(os.path.join(img_dir,'{}_walk_right{}.png'.format(tag,i))).convert()
+            self.imgs.append(pygame.transform.scale_by(img,sprite_scl))
+        for img in self.imgs:
+            img.set_colorkey(WHITE)
+        self.state=[2]
+        self.image=self.imgs[self.state[0]]
         self.rect=self.image.get_rect() # specify bounding rect for sprite
         # set hitbox
+        self.offset=14
         r=self.rect
-        self.hitbox=(r.x+r.w*0.3,r.y+r.h*0.24,r.w*0.4,r.w*0.65)
+        self.hitbox=pygame.Rect(r.x+r.w*0.33,r.y+r.h*0.14,r.w*0.34,r.w*0.68)
+        self.hitbox.center=(winWidth/2,floor-self.hitbox.h/2)
+        self.rect.center=(self.hitbox.centerx,self.rect.centery-self.offset)
         #pygame.draw.rect(self.image,RED,self.hitbox,width=1)
         # set random start position
         self.rect.x=0 # starting x-coordinate #random.randrange(winWidth-self.rect.width)
@@ -101,16 +159,52 @@ class Mob(pygame.sprite.Sprite):
         # set default speed
         self.speed_x=0 #random.randrange(-3,3) # random speed along the x-axis
         self.speed_y=0 #random.randrange(1,7) # random speed along the y-axis
+        self.walking=0
+        # times of last mob events
+        self.last_jump=0
+        self.last_step=0
     def update(self):
         # move
-        self.rect.x+=self.speed_x
-        self.rect.y+=self.speed_y
-        # check if enemy sprite leaves the bottom of the game window - then randomise at the top...
-        #if self.rect.top>winHeight+15 or self.rect.left<-15 or self.rect.right>winWidth+15:
+        if self.walking==0:
+            rand=random.randrange(0,sprite_activity)
+        else:
+            rand=random.randrange(0,sprite_activity//5)
+        now=pygame.time.get_ticks()
+        if self.hitbox.bottom<floor:
+            self.speed_y+=gravity
+        elif now-self.last_jump>2*delay and rand==1: # jump
+            self.speed_y=-player_jump_strength
+            self.last_jump=now
+        if rand<10: # attempt to change walking state
+            self.walking=rand%3-1 #(self.walking+2-(3*rand%2))%3-1
+        self.speed_x=self.walking*player_speed
+        if self.walking==0:
+            self.state=[2]
+        elif self.walking<0 and now-self.last_step>delay:
+            self.state[0]=(self.state[0]+1)%2
+        elif self.walking>0 and now-self.last_step>delay:
+            self.state[0]=self.state[0]%2+3
+        self.image=self.imgs[self.state[0]]
+        self.hitbox.x+=self.speed_x
+        self.hitbox.y+=self.speed_y
+        if self.hitbox.right>winWidth:
+            self.hitbox.right=winWidth
+            self.walking=0
+        if self.hitbox.left<0:
+            self.hitbox.left=0
+            self.walking=0
+        if self.hitbox.bottom>floor:
+            self.hitbox.bottom=floor
+            self.speed_y=0
+        if self.hitbox.top<0: # shouldn't be possible but just in case
+            self.hitbox.top=0
+        self.rect.center=self.hitbox.center
+        self.rect.centery-=self.offset
+
 
 # create a mob object
-def createMob():
-    mob=Mob()
+def createMob(tag):
+    mob=Mob(tag)
     game_sprites.add(mob) # add to game_sprites group to get object updated
     mob_sprites.add(mob) # add to mob_sprites group - use for collision detection &c.
 
@@ -136,17 +230,18 @@ bg_rect1=bg_img1.get_rect()
 bg0_x=-(bg_rect0.w-winWidth)/2 # starting x offset of the first background layer
 bg1_x=-(bg_rect1.w-winWidth)/2 # starting x offset of the second background layer
 # player
-player_img=pygame.image.load(os.path.join(img_dir,"DP_still.png")).convert()
-# npcs
-npc_imgs=[]
-npc_list=["MrRat.png","MsNymph.png"]
-for img in npc_list:
-    npc_imgs.append(pygame.image.load(os.path.join(img_dir,img)).convert())
+#player_img=pygame.image.load(os.path.join(img_dir,"DP_still.png")).convert()
 
 # sprite groups - game, mob, projectiles...
 game_sprites=pygame.sprite.Group()
 mob_sprites=pygame.sprite.Group()
-createMob() # create a new npc object
+#createMob() # create a new npc object
+# npcs
+npc_list=["MrRat"]#"MsNymph"
+#npc_imgs=[]
+#for img in npc_list:
+#    npc_imgs.append(pygame.image.load(os.path.join(img_dir,img)).convert())
+createMob(random.choice(npc_list))
 player=Player() # create player object
 game_sprites.add(player) # add an npc to game
 
@@ -161,7 +256,7 @@ while running:
         if event.type==pygame.QUIT:
             gameExit()
         if event.type==pygame.MOUSEBUTTONDOWN:
-            mouse_x,mouse_y=pygame.mouse.get_pos()
+            print(pygame.mouse.get_pos())
         if event.type==pygame.KEYDOWN:
             # check for ESCAPE key
             if event.key==pygame.K_ESCAPE:
@@ -169,7 +264,7 @@ while running:
     now=pygame.time.get_ticks()
     key_state = pygame.key.get_pressed()
     # check keyboard events - keydown
-    if any(key_state) and now-last_input>input_delay:
+    if any(key_state) and now-last_input>delay:
         last_input=pygame.time.get_ticks()
         # stop and start the train moving animation
         if key_state[pygame.K_SPACE]:
@@ -197,3 +292,4 @@ while running:
     window.blit(bg_img1,(bg1_x,0)) # layer 2
     game_sprites.draw(window) # draw all sprites to the game window
     pygame.display.update() # update the display window...
+# floor=572
